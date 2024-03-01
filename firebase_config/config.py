6c5012b0f41a02af331a88d997070a4bf11d28a8
Dennis614908 <dennisthairu900@gmail.com>
2024-03-01 18:24:53 +0300

    updated requirements.txt

--- firebase_config/config.py
+++ firebase_config/config.py
@@ -2,7 +2,7 @@ import firebase_admin
 from firebase_admin import credentials, storage, firestore
 
 
-cred = credentials.Certificate("firebase_config/credentials.json")
+cred = credentials.Certificate("credentials.json")
 firebase_admin.initialize_app(cred, {'storageBucket': 'chatify--chat.appspot.com'})
 
 bucket = storage.bucket()
